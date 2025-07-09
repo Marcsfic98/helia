ccd3274a90464bc171d14da899c068b5c6750f00
Marcos Ribeiro <131191211+Marcsfic98@users.noreply.github.com>
2025-07-09 12:12:47 -0300

    cards finalizados

--- src/components/AptList/index.tsx
+++ src/components/AptList/index.tsx
@@ -8,10 +8,12 @@ export function AptList() {
     <View style={styles.container}>
         <View style={styles.card}>
             <Pressable>
-                <Image source={require("../../../assets/images/ap1.jpg")} style={styles.cardImage}/>
-                <View  style={styles.cardInfo}>
-                    <Text style={styles.cardInfoTitle}>Hotel Miozine</Text>
-                    <Text style={styles.cardInfoSbTitle}>Recife-PE</Text>
+                <View style={styles.cardButton}>
+                    <Image source={require("../../../assets/images/ap1.jpg")} style={styles.cardImage}/>
+                    <View  style={styles.cardInfo}>
+                        <Text style={styles.cardInfoTitle}>Hotel Miozine</Text>
+                        <Text style={styles.cardInfoSbTitle}>Recife-PE</Text>
+                    </View>
                 </View>
             </Pressable>
         </View>
--- src/components/AptList/styles.ts
+++ src/components/AptList/styles.ts
@@ -1,10 +1,43 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  container: {},
-  card:{},
-  cardImage:{},
-  cardInfo:{},
-  cardInfoTitle:{},
-  cardInfoSbTitle:{}
+  container: {
+    width:"100%",
+    gap:20
+  },
+  cardButton:{
+    alignItems:"flex-start",
+    flexDirection:"row",
+    gap:10
+  },
+  card:{
+    width:"100%",
+    height:120,
+    borderRadius:12,
+    backgroundColor:"#1f222a",
+    paddingHorizontal:10,
+    alignItems:"center",
+    flexDirection:"row",
+    gap:10,
+    justifyContent:"space-between",
+  },
+  cardImage:{
+    width:90,
+    height:90,
+    borderRadius:12
+  },
+  cardInfo:{
+    height:"100%",
+    gap:10
+  },
+  cardInfoTitle:{
+    color:"#f4f4f4",
+    fontSize:16,
+    fontWeight:400
+  },
+  cardInfoSbTitle:{
+    color:"#f4f4f4",
+    fontSize:14,
+    fontWeight:300
+  }
 });
